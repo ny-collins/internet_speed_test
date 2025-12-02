743b85d67d270b49e2e80357fb42fed3cb1fa9b1
Collins Mwangi <mwangicollins391@gmail.com>
2025-12-02 22:15:14 +0300

    CSP micro-optimization: Remove Railway wildcard from connect-src

--- frontend/server.js
+++ frontend/server.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
         "style-src 'self'; " +
         "img-src 'self' data: https:; " +
         "font-src 'self' data:; " +
-        "connect-src 'self' https://speed-test-backend.up.railway.app https://*.railway.app https://unpkg.com; " +
+        "connect-src 'self' https://speed-test-backend.up.railway.app https://unpkg.com; " +
         "worker-src 'self' blob:; " +
         "frame-ancestors 'none'; " +
         "base-uri 'self'; " +
